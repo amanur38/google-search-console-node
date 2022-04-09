65a00bdc98e9b90396b6ba5add1e164bd4a71043
Amanur Rahman <amanur38@gmail.com>
2022-04-10 03:31:33 +0600

    Add them to github

---
+++ README.md
@@ -0,0 +1,35 @@
+# Welcome to google search console index API example in node js API
+
+Clone this repository form [here](https://github.com/amanur38/google-search-console-node)
+
+### setup google service account
+
+`Please create a service account without any permission or role then add this account to google search console under a domain.`
+
+### RUN
+
+```
+npm install
+or
+yar  install
+
+npm run dev
+or
+yarn dev
+
+```
+
+### Build
+
+```
+npm run build
+or
+yarn build
+
+```
+
+```
+npm start
+or
+yarn start
+```
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,86 @@
-var request = require("request");
-var { google } = require("googleapis");
-var key = require("./service_account.json");
+import "dotenv/config";
+import request from "request";
+import { google } from "googleapis";
+import mysql from "mysql";
 
+// import a service account from /src directory
+import key from "./service_account.json";
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "password",
+  database: process.env.DB_NAME || "eiinbd",
+});
+
+var totalRow = 0;
+var currentRow = 0;
+
+// Generate JTW access token
 const jwtClient = new google.auth.JWT(
   key.client_email,
-  null,
+  undefined,
   key.private_key,
-  ["https://www.googleapis.com/auth/indexing"],
-  null
+  [
+    "https://www.googleapis.com/auth/indexing",
+    // "https://www.googleapis.com/auth/webmasters",
+    // "https://www.googleapis.com/auth/webmasters.readonly",
+  ],
+  undefined
 );
 
-jwtClient.authorize(function (err: any, tokens: any) {
+// Authorize and get the access token
+jwtClient.authorize(async (err, tokens) => {
   if (err) {
     console.log(err);
     return;
   }
+
+  // If get the access token then call to database that
+  // these url was not request to index, so get these url with set a limit
+  connection.query(
+    "SELECT * from InstituteListClone where isIndex = false order by id desc limit 2 ",
+    async function (error: any, results: any, fields: any) {
+      if (error) throw error;
+      const parsedData = JSON.parse(JSON.stringify(results));
+      totalRow = parsedData.length;
+      for (const data of parsedData) {
+        const id = data.id;
+        const url = `https://eiinbd.howdo.live/${data.slug}`;
+        // Check if this url requested for index
+        checkAndRequestToIndex(url, tokens, id);
+        currentRow += 1;
+      }
+    }
+  );
+
+  // connection.end();
+});
+
+const checkAndRequestToIndex = (url: string, tokens: any, id: string) => {
+  const urlEncode = encodeURI(url);
+
+  let getOptions = {
+    url: `https://indexing.googleapis.com/v3/urlNotifications/metadata?url=${urlEncode}`,
+    method: "GET",
+    // Your options, which must include the Content-Type and auth headers
+    headers: {
+      "Content-Type": "application/json",
+    },
+    auth: { bearer: tokens.access_token },
+  };
+  request(getOptions, function (error: any, response: any, body: any) {
+    // Handle the response
+    const parse = JSON.parse(body);
+    if (parse.error?.code == 404) {
+      // If the url was not request for index then call to the request to index
+      requestToIndex(urlEncode, tokens, id);
+    }
+    console.log(parse);
+  });
+};
+
+const requestToIndex = (url: string, tokens: any, id: string) => {
   let options = {
     url: "https://indexing.googleapis.com/v3/urlNotifications:publish",
     method: "POST",
@@ -25,12 +91,32 @@ jwtClient.authorize(function (err: any, tokens: any) {
     auth: { bearer: tokens.access_token },
     // Define contents here. The structure of the content is described in the next step.
     json: {
-      url: "http://example.com/jobs/42",
+      url: url,
       type: "URL_UPDATED",
     },
   };
-  request(options, function (error: any, response: any, body: any) {
+
+  request(options, async function (error: any, response: any, body: any) {
     // Handle the response
     console.log(body);
+    try {
+      const parse = JSON.parse(body);
+      // If the request will success then update the column isIndex to true
+      if (parse.body?.code == 200) {
+        await connection.query(
+          "UPDATE InstituteListClone set isIndex = ? where id = ?",
+          [true, id],
+          function (error: any, results: any, fields: any) {
+            if (error) throw error;
+          }
+        );
+        if (totalRow == currentRow) {
+          connection.end();
+        }
+      }
+    } catch (error) {
+      console.log("🚀 ~ file: index.ts ~ line 118 ~ error", error);
+      connection.end();
+    }
   });
-});
+};
